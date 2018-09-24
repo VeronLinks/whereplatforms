7408e3e2785320baa2f5ee2b2f73790591ec4552
Jack <a00241893@student.ait.ie>
2018-09-25 00:37:16 +0100

    I created a demo scene with a platform that moves towards waypoints.

---
+++ wherePlatformsAt/Assets/Scripts/PlatformMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMovement : MonoBehaviour {
+
+    public float speed = 1.0f;
+    public GameObject[] waypoints;
+    public int waypointIndex = 0;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float speedDelta = speed * Time.deltaTime;
+
+        Vector3 platformPosition = transform.position;
+        Vector3 target = waypoints[waypointIndex].transform.position;
+        Vector3 rangeToClose = target - platformPosition;
+        Debug.DrawRay(transform.position, rangeToClose, Color.cyan);
+
+        float distance = Mathf.Sqrt(rangeToClose.x * rangeToClose.x + rangeToClose.y * rangeToClose.y);
+
+        float newx = rangeToClose.x / distance;
+        float newy = rangeToClose.y / distance;
+
+        Vector3 norm = new Vector3(newx, newy);
+
+        if (distance > speedDelta)
+        {
+            transform.Translate(norm * speedDelta);
+        }
+        else
+        {
+            waypointIndex++;
+        }
+
+        if (waypointIndex >= waypoints.Length)
+        {
+            waypointIndex = 0;
+        }
+    }
+}
