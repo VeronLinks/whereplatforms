3b74c552d3b0d00cfc06aa3f5bcc54ab218ce9b7
Jack <a00241893@student.ait.ie>
2018-09-26 18:59:12 +0100

    Player attaches to a moving platform

---
+++ wherePlatformsAt/Assets/Scripts/Platform/AttachToPlatform.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttachToPlatform : MonoBehaviour
+{
+    public CharacterController charPlayer;
+    public PlatformMovement pM;
+    public GameObject Player;
+    
+    //void OnCollisionEnter(Collision other)
+    //{
+    //    charPlayer.Move(pM.direction.normalized * pM.speed * Time.deltaTime);
+    //}
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        //charPlayer.Move(pM.direction.normalized * pM.speed * Time.deltaTime);
+        if (other.gameObject == Player)
+        {
+            other.transform.parent = gameObject.transform;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == Player)
+        {
+            other.transform.parent = null;
+        }
+    }
+}
