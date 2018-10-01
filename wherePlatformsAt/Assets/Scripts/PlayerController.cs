fe1b94abb1bc58f275ee2d882f8b7a75b6a3682a
Jack <a00241893@student.ait.ie>
2018-10-01 17:03:47 +0100

    player instantiates a bullet

--- wherePlatformsAt/Assets/Scripts/PlayerController.cs
+++ wherePlatformsAt/Assets/Scripts/PlayerController.cs
@@ -3,9 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-    
+
+    public GameObject bullet;
+    private bool canFire = true;
+
     private CharController playerChar;
-    
+
+    void Fire()
+    {
+        // Instantiate a bullet on the screen
+        GameObject bulletClone = Instantiate(bullet, transform.position, transform.rotation);
+    }
+
     void Start()
     {
         playerChar = GetComponent<CharController>();
@@ -18,5 +27,18 @@ public class PlayerController : MonoBehaviour {
         playerChar.JumpTrigger = Input.GetKeyDown(KeyCode.Space);
 
         //playerChar.Attacking = Input.GetButtonDown("Fire1");
+        if (Input.GetMouseButton(0))
+        {
+            if (canFire)
+            {
+                canFire = false;
+                Fire();
+            }
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            canFire = true;
+        }
     }
 }
