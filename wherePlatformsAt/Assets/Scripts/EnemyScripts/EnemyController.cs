12b0130b9a5d533b8fa9bfa5007fb733916a822e
Cian <34064542+A00240928@users.noreply.github.com>
2018-10-01 17:08:50 +0100

    enemy fire

---
+++ wherePlatformsAt/Assets/Scripts/EnemyScripts/EnemyController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+
+    private Rigidbody myRB;
+    public float moveSpeed;
+
+    public PlayerController thePlayer;
+
+    private float timeBtwShots;
+    public float startTimeBtwShots;
+    public GameObject bullet;
+    public Transform firePosition;
+
+	// Use this for initialization
+	void Start ()
+    {
+        myRB = GetComponent<Rigidbody>();
+        thePlayer = FindObjectOfType<PlayerController>();
+
+        timeBtwShots = startTimeBtwShots;
+	}
+	private void FixedUpdate()
+    {
+        myRB.velocity = (transform.forward * moveSpeed);
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.LookAt(thePlayer.transform.position);
+
+        if(timeBtwShots <= 0)
+        {
+            Instantiate(bullet, firePosition.position, Quaternion.identity);
+            timeBtwShots = startTimeBtwShots;
+        }
+        else
+        {
+            timeBtwShots -= Time.deltaTime;
+        }
+	}
+
+}
