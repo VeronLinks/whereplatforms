46ebf4bb54d09031d7efebc31ae503c46b0bc2e5
Cian <34064542+A00240928@users.noreply.github.com>
2018-10-03 15:23:33 +0100

    merge

--- wherePlatformsAt/Assets/Scripts/Characters/PlayerController.cs
+++ wherePlatformsAt/Assets/Scripts/Characters/PlayerController.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
     public GameObject bullet;
+    public GameObject Paint;
+    public Transform firePoint;
     private bool canFire = true;
+    private int ammo = 0;
 
     private CharController playerChar;
 
     void Fire()
     {
         // Instantiate a bullet on the screen
-        GameObject bulletClone = Instantiate(bullet, transform.position, transform.rotation);
+        GameObject bulletClone = Instantiate(bullet, firePoint.transform.position, firePoint.transform.rotation);
     }
 
     void Start()
@@ -27,18 +30,28 @@ public class PlayerController : MonoBehaviour {
         playerChar.JumpTrigger = Input.GetKey(KeyCode.Space);
 
         //playerChar.Attacking = Input.GetButtonDown("Fire1");
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButton(0) && ammo > 0)
         {
             if (canFire)
             {
                 canFire = false;
                 Fire();
+                ammo--;
             }
         }
 
         if (Input.GetMouseButtonUp(0))
         {
             canFire = true;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Paint")
+        {
+            Destroy(other.gameObject);
+            ammo += 5;
         }
     }
 }
--- wherePlatformsAt/Assets/Scripts/Shooting/bullet.cs
+++ wherePlatformsAt/Assets/Scripts/Shooting/bullet.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class bullet : MonoBehaviour {
 
-    private float speed1 = 1.0f;
+    private GameObject player;
+    private Transform target;
+    private float speed1 = 10.0f;
+    Vector3 direction;
+    private Vector3 target2;
+
     // Use this for initialization
-    void Start () {}
+    void Start () {
+        target = GameObject.FindGameObjectWithTag("Target").transform;
+        target2 = new Vector3(target.position.x, target.position.y, target.position.z);
+    }
 	
 	// Update is called once per frame
 	void Update () {
 
-        transform.Translate(transform.forward * speed1);
+        //transform.Translate(player.transform.forward * speed1);
+        transform.position = Vector3.MoveTowards(transform.position, target2, speed1 * Time.deltaTime);
     }
 }
