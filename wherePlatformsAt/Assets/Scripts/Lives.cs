ffe5fcb9c0a1cbb34caf553f5d442b2a13c8f2c1
Mac <A00242722@student.ait.ie>
2018-10-08 16:53:50 +0100

    Lose Scrren

---
+++ wherePlatformsAt/Assets/Scripts/Lives.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Lives : MonoBehaviour {
+    private int lives = 3;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Ground(Lava)")
+        {
+            lives--;
+        }
+
+    }
+    public void death()
+    {
+        if (lives < 2)
+        {
+            SceneManager.LoadScene("lose");
+        }
+    }
+private void OnGUI()
+    {
+        GUI.Box(new Rect(10,50, 100, 30), "Lives: " +lives);
+    }
+}
