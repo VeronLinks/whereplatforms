092de9e185fb3c7415696671c4e1658f9cc721fc
Jack <a00241893@student.ait.ie>
2018-10-15 16:59:38 +0100

    Implemented splatter on invisible platforms

--- wherePlatformsAt/Assets/Scripts/bullet.cs
+++ wherePlatformsAt/Assets/Scripts/bullet.cs
@@ -7,16 +7,30 @@ public class bullet : MonoBehaviour {
     private Transform target;
     private float speed1 = 14.0f;
     private Vector3 direction;
+    public GameObject splatter;
+    public int seconds;
 
     // Use this for initialization
     void Start () {
         target = GameObject.FindGameObjectWithTag("Target").transform;
         direction = new Vector3(target.position.x, target.position.y, target.position.z);
+        Destroy(gameObject, seconds);
     }
 	
 	// Update is called once per frame
 	void Update () {
 
         transform.position = Vector3.MoveTowards(transform.position, direction, speed1 * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {
+        Vector3 pos = transform.position;
+        pos.y += 0.1f;
+        if (col.gameObject.tag == "Platform")
+        {
+            Instantiate(splatter, pos, transform.rotation);
+            Destroy(this.gameObject);
+        }
     }
 }
