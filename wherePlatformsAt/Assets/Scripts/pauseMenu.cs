954f1262b881789f7b0a2fad0e343d7249f0a1b4
Cian <34064542+A00240928@users.noreply.github.com>
2018-11-07 13:19:39 +0000

    changed menu button from 'm' to 'tab'

--- wherePlatformsAt/Assets/Scripts/pauseMenu.cs
+++ wherePlatformsAt/Assets/Scripts/pauseMenu.cs
@@ -9,7 +9,7 @@ public class pauseMenu : MonoBehaviour
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetKeyDown(KeyCode.M))
+		if(Input.GetKeyDown(KeyCode.Tab))
         {
             if(GameIsPaused)
             {
