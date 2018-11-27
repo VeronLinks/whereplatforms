8512bf4c6d8dc004a66f0ac8441d261a1c9402ec
Conor Macken <A00242722@student.ait.ie>
2018-11-27 22:10:03 +0000

    leaderboard

---
+++ wherePlatformsAt/Assets/Scripts/GameController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    
+    time = PlayerPrefs.GetFloat("Timer", 0)
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<LeaderBoard>().checkForTime(time);
+    }
+}
